cc1061a46c19b0ef28f57ca74c232a726015f8d2
Kiran-512 <kiranyadav267@gmail.com>
2023-03-05 13:16:00 +0530

    typescript demo

---
+++ app.js
@@ -0,0 +1,3 @@
+var a = 'Kiran'; // loosely typed
+var b = 'String demo'; // stringly typed
+console.log(a);
---
+++ app.ts
@@ -0,0 +1,3 @@
+var a = 'Kiran' // loosely typed
+var b: string = 'String demo' // stringly typed
+console.log(a)
